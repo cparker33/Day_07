308b10167418b80d2dbe18e74469182670038060
Caleb Parker <caleb_iphone2@yahoo.com>
2017-03-08 19:58:29 -0800

    From Home

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 // code goes here :)
 
+
+// #############################
+//  Question 1 cItPrice
+// #############################
+
 var  cItPrice = (items.reduce(function(a, b) {
 
   return a + b.price 
 
 }, 0) / items.length).toFixed(2)
 
+document.querySelector("#answer1").innerHTML = `
+The price is $${cItPrice}
+`
+
+
+
+
 // #############################
-//  Question 1 cItPrice
+//  Question 2 cItPriceRange
 // #############################
 
 
@@ -24,9 +36,27 @@ var cItPriceRange = items.filter(function(val) {
     return val.title
 })
 
+
+var cAns2 = document.querySelector("#answer2")
+
+var cAns2Str = ""
+
+cItPriceRange.forEach(function(val){
+
+  cAns2Str += (val + "<br><br>")
+
+})
+
+cAns2.innerHTML = cAns2Str;
+
+
+
+// ############################# 
+//  Question 3 **        &pound;
+// console.log(cItGbp[0].title)
+// console.log(cItGbp[0].price)
 // #############################
-//  Question 2 cItPriceRange
-// #############################
+
 
 
 var cItGbp = items.filter(function(val) { // filter for 'GBP'
@@ -45,14 +75,23 @@ var cItGbp = items.filter(function(val) { // filter for 'GBP'
     }
 })
 
-// call with index 0 and key
+var cAns3 = document.querySelector("#answer3")
+
+var cAns3Str = ""
+
+cAns3Str += (cItGbp[0].title)
+cAns3Str += (" &pound;" + cItGbp[0].price)
+
+cAns3.innerHTML = cAns3Str;
+
 
 
 // ############################# 
-//  Question 3 **        &pound;
-// console.log(cItGbp[0].title)
-// console.log(cItGbp[0].price)
+//  Question 4 
+// console.log(cItWood)
 // #############################
+
+
 
 var cItWood = items.filter(function(val) { 
 
@@ -68,12 +107,43 @@ var cItWood = items.filter(function(val) {
     return val.title
 })
 
+
+var cAns4 = document.querySelector("#answer4")
+
+var cAns4Str = ""
+
+cItWood.forEach(function(val){
+
+  cAns4Str += (val + " is made of wood.\n")
+
+})
+
+
+
+
+
+var cAns4 = document.querySelector("#answer4")
+
+var cAns4Str = ""
+
+cItWood.forEach(function(val){
+
+  cAns4Str += (val + " is made of wood.\n")
+
+})
+
+cAns4.innerHTML = cAns4Str;
+
+
+
+
+
 // ############################# 
-//  Question 4 
-console.log(cItWood)
+//  Question 5 
+// console.log(cIt8More[0].title)
+// console.log(cIt8More[0].materials)
+// console.log(cIt8More.length)
 // #############################
-
-
 
 
 var cIt8More = items.filter(function(val) { 
@@ -90,102 +160,6 @@ var cIt8More = items.filter(function(val) {
 
 
 })
-
-
-// ############################# 
-//  Question 5 
-// console.log(cIt8More[0].title)
-// console.log(cIt8More[0].materials)
-// console.log(cIt8More.length)
-// #############################
-
-
-
-var cItSelfMade = items.filter(function(val) { 
-
-
-  if (val.who_made === 'i_did') {
-
-    return val
-
-  }
-
-}).length
-
-//console.log(cItSelfMade)
-// used_manufacturer
-
-
-
-
-
-
-
-
-
-
-
-document.querySelector("#answer1").innerHTML = `
-The price is $${cItPrice}
-`
-
-
-
-var cAns2 = document.querySelector("#answer2")
-
-var cAns2Str = ""
-
-cItPriceRange.forEach(function(val){
-
-  cAns2Str += (val + "\n")
-
-})
-
-cAns2.innerHTML = cAns2Str;
-
-
-
-// ############################# 
-//  Question 3 **        &pound;
-// console.log(cItGbp[0].title)
-// console.log(cItGbp[0].price)
-// #############################
-
-var cAns3 = document.querySelector("#answer3")
-
-var cAns3Str = ""
-
-cAns3Str += (cItGbp[0].title)
-cAns3Str += (" &pound;" + cItGbp[0].price)
-
-cAns3.innerHTML = cAns3Str;
-
-
-// ############################# 
-//  Question 4 
-// console.log(cItWood)
-// #############################
-
-
-var cAns4 = document.querySelector("#answer4")
-
-var cAns4Str = ""
-
-cItWood.forEach(function(val){
-
-  cAns4Str += (val + " is made of wood.\n")
-
-})
-
-cAns4.innerHTML = cAns4Str;
-
-
-// ############################# 
-//  Question 5 
-// console.log(cIt8More[0].title)
-// console.log(cIt8More[0].materials)
-// console.log(cIt8More.length)
-// #############################
 
 
 var cAns5 = document.querySelector("#answer5")
@@ -213,6 +187,23 @@ cAns5.innerHTML = cAns5Str;
 
 
 
+// ############################# 
+//  Question 5 
+//console.log(cItSelfMade)
+// ############################# 
+
+var cItSelfMade = items.filter(function(val) { 
+
+
+  if (val.who_made === 'i_did') {
+
+    return val
+
+  }
+
+}).length
+
+
 document.querySelector("#answer6").innerHTML = `
 
 ${cItSelfMade} were made by their sellers.
@@ -220,12 +211,6 @@ ${cItSelfMade} were made by their sellers.
 `
 
 
-
-
-
-
-
-// cItSelfMade
 
 
 
